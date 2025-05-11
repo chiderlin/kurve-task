644a762b9a0fca9eef54978c56904d8786df6001
chi <chiderlin36@gmail.com>
2025-05-11 18:55:25 +0100

    backend logic init

---
+++ backend/db.ts
@@ -0,0 +1,84 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+let pool: mysql.Pool;
+
+export function connectToDB() {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+    console.log('Connected to the database');
+  }
+
+  return pool;
+}
+
+export async function insertCustomer(
+  pool: mysql.Pool,
+  customer: { name: string; email: string; age: number }
+) {
+  try {
+    const query = `INSERT INTO ${process.env.DB_NAME}.Customers (name, email, age) VALUES (?,?,?)`;
+    const [result] = await pool.execute(query, [
+      customer.name,
+      customer.email,
+      customer.age,
+    ]);
+    console.log('Inserted customer:', result);
+    return result;
+  } catch (error) {
+    console.error('Error inserting customer:', error);
+    throw error;
+  }
+}
+
+export async function getCustomers(pool: mysql.Pool) {
+  try {
+    const query = `SELECT * FROM ${process.env.DB_NAME}.Customers`;
+    const [rows] = await pool.execute(query);
+    return rows;
+  } catch (error) {
+    console.error('Error fetching customers:', error);
+    throw error;
+  }
+}
+export async function updateCustomer(
+  pool: mysql.Pool,
+  id: number,
+  customer: { name: string; email: string; age: number }
+) {
+  try {
+    const query = `UPDATE ${process.env.DB_NAME}.Customers SET name = ?, email = ?, age = ? WHERE id = ?`;
+    const [result] = await pool.execute(query, [
+      customer.name,
+      customer.email,
+      customer.age,
+      id,
+    ]);
+    console.log('Updated customer:', result);
+    return result;
+  } catch (error) {
+    console.error('Error updating customer:', error);
+    throw error;
+  }
+}
+
+export async function deleteCustomer(pool: mysql.Pool, id: number) {
+  try {
+    const query = `DELETE FROM ${process.env.DB_NAME}.Customers WHERE id = ?`;
+    const [result] = await pool.execute(query, [id]);
+    console.log('Deleted customer:', result);
+    return result;
+  } catch (error) {
+    console.error('Error deleting customer:', error);
+    throw error;
+  }
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,100 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import {
+  connectToDB,
+  insertCustomer,
+  getCustomers,
+  deleteCustomer,
+  updateCustomer,
+} from './db';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+const db = connectToDB();
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.get('/api/customers', async (req: Request, res: Response) => {
+  try {
+    const customers = await getCustomers(db);
+    res.status(200).json(customers);
+    return;
+  } catch (error) {
+    console.error('Error fetching customers:', error);
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+});
+
+app.post('/api/customers', async (req: Request, res: Response) => {
+  const { name, email, age } = req.body;
+  try {
+    if (!name || !email || !age) {
+      res.status(400).json({ error: 'Name, email, and age are required' });
+      return;
+    }
+    const rs = await insertCustomer(db, req.body);
+    console.log('rs', rs);
+    res.status(201).json({
+      message: 'Customer added successfully',
+    });
+    return;
+  } catch (error) {
+    console.error('Error inserting customer:', error);
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+});
+
+app.put('/api/customers/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { name, email, age } = req.body;
+  if (!name || !email || !age || !id) {
+    res.status(400).json({ error: 'Name, email, and age are required' });
+    return;
+  }
+  const _id = parseInt(id);
+  if (isNaN(_id)) {
+    res.status(400).json({ error: 'Invalid ID' });
+    return;
+  }
+
+  try {
+    const rs = await updateCustomer(db, _id, { name, email, age });
+    console.log('rs', rs);
+    res.status(200).json({
+      message: 'Customer updated successfully',
+    });
+    return;
+  } catch (error) {
+    console.error('Error updating customer:', error);
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+});
+
+app.delete('/api/customers/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const _id = parseInt(id);
+  if (isNaN(_id)) {
+    res.status(400).json({ error: 'Invalid ID' });
+    return;
+  }
+  try {
+    const rs = await deleteCustomer(db, _id);
+    console.log('rs', rs);
+    res.status(200).json({
+      message: 'Customer deleted successfully',
+    });
+    return;
+  } catch (error) {
+    console.error('Error deleting customer:', error);
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
 });
 
 app.listen(PORT, () => {
